968bdb611469a32b2ef688273428ba6c5b8e20cf
Dodik Dwi Sancoko <dodikds@gmail.com>
2023-06-09 15:53:02 +0700

    upload all project news-app

---
+++ app/Models/PostView.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostView extends Model
+{
+    use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'post_id', 'views'
+    ];
+}
