cf97349886e905b886abb58143ae0ca149f274f5
Dodik Dwi Sancoko <dodikds@gmail.com>
2023-06-22 14:49:19 +0700

    Membuat Layout Admin

---
+++ news-admin/src/routes/index.jsx
@@ -0,0 +1,29 @@
+//import react
+import React, { lazy, Suspense } from 'react';
+
+//import react router dom
+import { Routes, Route } from "react-router-dom";
+
+//import loader component
+const Loader = lazy(() => import('../components/Loader.jsx'));
+
+//import view Login
+const Login = lazy(() => import('../views/Auth/Login.jsx'));
+
+export default function RoutesIndex() {
+
+    return (
+        <Routes>
+
+            {/* route "/" */}
+            <Route
+                path="/"
+                element={
+                    <Suspense fallback={<Loader />}>
+                        <Login />
+                    </Suspense>
+                }
+            />
+        </Routes>
+    )
+}
